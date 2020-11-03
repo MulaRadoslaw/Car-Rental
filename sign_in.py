8ee6ad344570ee887c166467f2895669055b9bbb
MulaRadoslaw <72382265+MulaRadoslaw@users.noreply.github.com>
2020-11-03 11:03:31 +0100

    Update sign_in.py

--- sign_in.py
+++ sign_in.py
@@ -17,25 +17,18 @@ def signin():
         cursor = connection.cursor()
         cursor.execute(sql_select_Query)
         records = cursor.fetchall()
-        print("Total number of rows is: ", cursor.rowcount)
 
         login = input("login:")
-        print(records)
 
         for row in records:
 
             if login == row[1]:
-                print(row[2])
                 password = input("password:")
                 if password == row[2]:
                     user_name = row[3]
                     print("Logging in...\n")
 
                     zalogowano = True
-                    print("signin zalogowano?")
-
-                    print(zalogowano)
-
 
                 else:
                     pass
